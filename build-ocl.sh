e5298ac8c239eaed11692f53766e561c4b589b57
kevall474 <kevall474@tuta.io>
2021-02-26 18:55:41 -0500

    Update build-ocl.sh

--- build-ocl.sh
+++ build-ocl.sh
@@ -14,3 +14,17 @@
 cd ocl-icd-git && env _compiler=2 makepkg -si && cd ..
 
 cd lib32-ocl-icd-git && env _compiler=2 makepkg -si && cd ..
+
+# make a copy of every pkg in package-$(date -I)/ dir
+
+mkdir -p package-$(date -I)
+
+cp -v */package-$(date -I)/*.pkg.tar.zst package-$(date -I)/
+cp -v */*.pkg.tar.zst package-$(date -I)/
+
+# clean build dir
+
+rm -rf */src/
+rm -rf */pkg/
+rm -rf */*/src/
+rm -rf */*/pkg/
