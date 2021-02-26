abfb9f111296a8feb06a962497a6bc995ff68783
kevall474 <kevall474@tuta.io>
2021-02-26 18:52:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,6 +18,10 @@ cd lib32-spirv-cross-git && env _compiler=2 makepkg -si && cd ..
 
 cd lib32-spirv-tools-git && env _compiler=2 makepkg -si && cd ..
 
+# glslang
+
+cd glsalng-git && env _compiler=2 makepkg -si && cd ..
+
 # vulkan
 
 cd vulkan-headers-git && env _compiler=2 makepkg -si && cd ..
@@ -32,14 +36,28 @@ cd vulkan-tools-git && env _compiler=2 makepkg -si && cd ..
 
 cd vulkan-lunarg-tools-git && env _compiler=2 makepkg -si && cd ..
 
-cd lib32-vulkan-icd-loader-git && env _compiler=2 makepkg -si && cd ..
-
-cd lib32-vulkan-validation-layers-git && makepkg -si && cd ..
-
-cd lib32-vulkan-extensionlayers-git && env _compiler=2 makepkg -si && cd ..
-
 # opencl
 
 cd opencl-headers-git && env _compiler=2 makepkg -si && cd ..
 
 cd opencl-clhpp-git && env _compiler=2 makepkg -si && cd ..
+
+# lib32 #
+
+# spirv
+
+cd lib32-spirv-cross-git && env _compiler=2 makepkg -si && cd ..
+
+cd lib32-spirv-tools-git && env _compiler=2 makepkg -si && cd ..
+
+# glslang
+
+cd lib32-glsalng-git && env _compiler=2 makepkg -si && cd ..
+
+# vulkan
+
+cd lib32-vulkan-icd-loader-git && env _compiler=2 makepkg -si && cd ..
+
+cd lib32-vulkan-validation-layers-git && makepkg -si && cd ..
+
+cd lib32-vulkan-extensionlayers-git && env _compiler=2 makepkg -si && cd ..
